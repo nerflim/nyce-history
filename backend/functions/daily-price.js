d951e6c56ae2d8917b85fe0938072c7824e4ba4a
Milfren <nerflimjohn@gmail.com>
2019-09-08 16:16:17 +1000

    disable callback waiting

--- backend/functions/daily-price.js
+++ backend/functions/daily-price.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-
 let dailyPrice = require('../models/dailyPrice.model');
 
 exports.handler = function(event, context, callback) {
+	context.callbackWaitsForEmptyEventLoop = false;
+
 	// Send user response
 	const send = body => {
 		callback(null, {
